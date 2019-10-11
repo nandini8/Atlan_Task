bc05b2fcebe59fd948136c0d4ddf13d9a99a5244
nandini soni <nandini.soni8@gmail.com>
2019-10-11 21:17:08 +0530

    terminate/start/pause/resume working

--- project/resources/process_functions.py
+++ project/resources/process_functions.py
@@ -1,8 +1,9 @@
 """
 Program to terminate/pause/resume a process
 0 signifies TERMINATE
-1 signifies PAUSE
-2 signifies RESUME
+1 signifies START
+2 signifies PAUSE
+3 signifies RESUME
 """
 
 
@@ -16,18 +17,20 @@ def do_work(count):
     return count
 
 def worker(run, function, data):
-    count = 0
+    index = 0
     ch = True
     while ch:
-        if run.value == 2:
-            function(data)
+        if run.value == 3 or run.value == 1:
+            index = function(data)
+            print(index)
         elif run.value == 0:
             ch = False
             print("Terminator")
             return
-        elif run.value == 1:
-            print("Paused")
-            pass
+        elif run.value == 2:
+            sleep(1)
+            print("Paused", index)
+            
         
 def main_run(function, data, cancel):
     # run = Value("i", cancel)
@@ -41,8 +44,7 @@ def main_run(function, data, cancel):
             p.terminate()
             p.join()
             break
-        if run.value == 1:
-            print("AAAAAAAA")
+
 
 # if __name__ == '__main__':
 #     main_run()
--- project/resources/upload.py
+++ project/resources/upload.py
@@ -15,24 +15,44 @@ def data_pre_process(filename):
     path = os.path.join(UPLOAD_FOLDER, filename)
     file = pd.read_csv(path)
     data = file.to_dict('index')
-    return data
+    # print(data)
+    gen = ((key,value) for key,value in data.items())
+    return gen
+
+# def cache_to_redis(data):
+#     db = redis.Redis('localhost')
+    
+#     if data == None:
+#         return
+#     index = 0
+#     for key in data.keys():
+#         time.sleep(1)
+#         db.hmset(key, data[key])
+#         print("Running ", index)
+#         index += 1
+#     db.close()
+#     return index
 
 def cache_to_redis(data):
     db = redis.Redis('localhost')
-    # print(type(db), stop)
-    
     if data == None:
-        return
-    for key in data.keys():
         time.sleep(1)
-        db.hmset(key, data[key])
-        print("Running")
+        return
+    if cancel.value == 1 or cancel.value == 3:
+        time.sleep(1)
+        value = next(data)
+        db.hmset( value[0], value[1])
+        return value
+    else:
+        time.sleep(1)
+        return 0
 
     
-cancel = Value("i", 2)
+cancel = Value("i", 1)
 
 class upload(Resource):
     def get(self):
+        # print("What")
         pass
 
     def post(self):
@@ -40,19 +60,24 @@ class upload(Resource):
         cancel.value = int(request.form['cancel'] )
         data = None
         
-        if file and cancel.value == 2:
+        if file and cancel.value == 1:
             
             file.save(os.path.join(UPLOAD_FOLDER, file.filename))
             data = data_pre_process(file.filename)
+            send_data = Process(target=main_run, args=(cache_to_redis, data, cancel, ))
+            send_data.start()
            
         # else:
         if cancel.value == 0:
-            print("No file uploaded")
-        if cancel.value == 1:
-            print("Paused")
+            return redirect(url_for('api.upload'))
 
-        send_data = Process(target=main_run, args=(cache_to_redis, data, cancel, ))
-        send_data.start()
+        if cancel.value == 2:
+            print("Paused")
+        
+        if cancel.value == 3:
+            print("Resumed")
+
+        
         return redirect(url_for('api.upload'))
         # save_data_in_db(os.path.join(UPLOAD_FOLDER, file.filename))
         
