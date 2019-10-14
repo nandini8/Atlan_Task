57541733243300942ba433e958a72939f710cf82
nandini soni <nandini.soni8@gmail.com>
2019-10-14 18:29:12 +0530

    Replaced files

--- project/start.sh
+++ project/start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 app="docker.api"
 docker build -t ${app} .
-docker run -d -p 56733:80 \
-  --name=${app} \
-  -v $PWD:/app ${app}
+docker run -d -p 56733:80 --name=${app} -v $PWD:/app ${app}
